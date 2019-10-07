518338aa685b841129a75069487f844dab7b7043
Wojciech Domagala <wojciech.domagala@almservices.pl>
2019-10-07 12:05:40 +0200

    First commit

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About this store</h2>
+    <p>I wrote this app in ALM Academy to learn Node.js and react.js</p>
+  </div>
+);
+
+export default AboutPage;
---
+++ src/components/common/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.activeStyle = { color: "#F15B2A" };
+  }
+
+  render() {
+    return (
+      <nav>
+        <NavLink to="/" activeStyle={this.activeStyle} exact>
+          Home
+        </NavLink>{" "}
+        {"  |  "}
+        <NavLink to="/about" activeStyle={this.activeStyle}>
+          About
+        </NavLink>
+      </nav>
+    );
+  }
+}

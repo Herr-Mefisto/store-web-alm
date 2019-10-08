2621865e23907bd86bf62c77a60163046aa0caf4
Wojciech Domagala <wojciech.domagala@almservices.pl>
2019-10-08 11:26:07 +0200

    Introduced REDUX

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 export default class PageNotFound extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return <h1>Ooops this page does not exists</h1>;
   }
--- src/components/products/ProductsPage.js
+++ src/components/products/ProductsPage.js
@@ -1,9 +1,60 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as productsActions from "../../redux/actions/productActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 
 class ProductsPage extends React.Component {
+  state = { product: { title: "" } };
+
+  handleChange = event => {
+    const product = { ...this.state.product, title: event.target.value };
+    this.setState({ product });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.actions.createProduct(this.state.product);
+  };
+
   render() {
-    return <h2>Products</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Products</h2>
+        <h3>Add product</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.product.title}
+        />
+        <input type="submit" value="Save" />
+
+        {this.props.products.map(product => (
+          <div key={product.title}>{product.title}</div>
+        ))}
+      </form>
+    );
   }
 }
 
-export default ProductsPage;
+ProductsPage.propTypes = {
+  actions: PropTypes.object.isRequired,
+  products: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    products: state.products
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(productsActions, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProductsPage);
